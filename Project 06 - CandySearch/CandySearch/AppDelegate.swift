e0bb49a8b01898dc816914453423d3c42502aa1e
Yi Gu <yi@superfamily.me>
2016-04-21 18:47:45 -0700

    [04/21] Proj06: implement Search Controller and Scope Bar for Candy Search

---
+++ Project 06 - CandySearch/CandySearch/AppDelegate.swift
@@ -0,0 +1,60 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+  
+  var window: UIWindow?
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    // Override point for customization after application launch.
+    let splitViewController = window!.rootViewController as! UISplitViewController
+    let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+    navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
+    splitViewController.delegate = self
+    
+    UISearchBar.appearance().barTintColor = UIColor.candyGreen()
+    UISearchBar.appearance().tintColor = UIColor.whiteColor()
+    UITextField.appearanceWhenContainedInInstancesOfClasses([UISearchBar.self]).tintColor = UIColor.candyGreen()
+    return true
+  }
+  
+  // MARK: - Split view
+  func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
+    guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+    guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+    if topAsDetailController.detailCandy == nil {
+      // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+      return true
+    }
+    return false
+  }
+  
+}
+
+extension UIColor {
+  static func candyGreen() -> UIColor {
+    return UIColor(red: 67.0/255.0, green: 205.0/255.0, blue: 135.0/255.0, alpha: 1.0)
+  }
+}
---
+++ Project 06 - CandySearch/CandySearch/MasterViewController.swift
@@ -0,0 +1,144 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+  
+  // MARK: - Properties
+  var detailViewController: DetailViewController? = nil
+  var candies = [Candy]()
+  var filteredCandies = [Candy]()
+  let searchController = UISearchController(searchResultsController: nil)
+  
+  // MARK: - View Setup
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    candies = [
+        Candy(category:"Chocolate", name:"Chocolate Bar"),
+        Candy(category:"Chocolate", name:"Chocolate Chip"),
+        Candy(category:"Chocolate", name:"Dark Chocolate"),
+        Candy(category:"Hard", name:"Lollipop"),
+        Candy(category:"Hard", name:"Candy Cane"),
+        Candy(category:"Hard", name:"Jaw Breaker"),
+        Candy(category:"Other", name:"Caramel"),
+        Candy(category:"Other", name:"Sour Chew"),
+        Candy(category:"Other", name:"Gummi Bear")
+    ]
+    
+    setupSearchController()
+
+    if let splitViewController = splitViewController {
+      let controllers = splitViewController.viewControllers
+      detailViewController = (controllers[controllers.count - 1] as! UINavigationController).topViewController as? DetailViewController
+    }
+  }
+  
+  // MARK: - Search Controller Setup
+  func setupSearchController () {
+    searchController.searchResultsUpdater = self
+    searchController.dimsBackgroundDuringPresentation = false
+    definesPresentationContext = true
+    tableView.tableHeaderView = searchController.searchBar
+    searchController.searchBar.scopeButtonTitles = ["All", "Chocolate", "Hard", "Other"]
+    searchController.searchBar.delegate = self
+  }
+  
+  func filterContentForSearchText(searchText: String, scope: String = "All") {
+    filteredCandies = candies.filter { candy in
+      let categoryMatch = (scope == "All") || (candy.category == scope)
+      return  categoryMatch && candy.name.lowercaseString.containsString(searchText.lowercaseString)
+    }
+    
+    tableView.reloadData()
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    clearsSelectionOnViewWillAppear = splitViewController!.collapsed
+    super.viewWillAppear(animated)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  // MARK: - Table View
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if searchController.active && searchController.searchBar.text != "" {
+      return filteredCandies.count
+    }
+    return candies.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    
+    let candy: Candy
+    if searchController.active && searchController.searchBar.text != "" {
+      candy = filteredCandies[indexPath.row]
+    } else {
+      candy = candies[indexPath.row]
+    }
+    cell.textLabel!.text = candy.name
+    cell.detailTextLabel!.text = candy.category
+    return cell
+  }
+  
+  // MARK: - Segues
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "showDetail" {
+      if let indexPath = tableView.indexPathForSelectedRow {
+        let candy: Candy
+        if searchController.active && searchController.searchBar.text != "" {
+          candy = filteredCandies[indexPath.row]
+        } else {
+          candy = candies[indexPath.row]
+        }
+        let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+        controller.detailCandy = candy
+        controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem()
+        controller.navigationItem.leftItemsSupplementBackButton = true
+      }
+    }
+  }
+  
+}
+
+extension MasterViewController: UISearchResultsUpdating {
+  func updateSearchResultsForSearchController(searchController: UISearchController) {
+    let searchBar = searchController.searchBar
+    let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+    filterContentForSearchText(searchController.searchBar.text!, scope: scope)
+  }
+}
+
+extension MasterViewController: UISearchBarDelegate {
+  func searchBar(searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+    filterContentForSearchText(searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+  }
+}
+
