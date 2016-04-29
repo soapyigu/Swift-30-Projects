3d7c64934a249eb9902a72d48c027e0c045f8159
soapyigu <rio.deliberate@gmail.com>
2016-04-28 23:15:38 -0700

    [04/28] Proj 10: implement UI for Spotify

---
+++ Project 10 - SpotifySignIn/SpotifySignIn/MasterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MasterViewController.swift
+//  SpotifySignIn
+//
+//  Created by Yi Gu on 4/28/16.
+//  Copyright © 2016 YiGu. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
