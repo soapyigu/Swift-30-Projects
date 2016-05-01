dd7a1c7ed3cc2b3f426ce231925a2ae8f1fc3cc6
soapyigu <rio.deliberate@gmail.com>
2016-04-30 22:31:30 -0700

    [04/30] Proj 09: Implement basic tableview for animations

--- Project 09 - Animations/Animations/ViewController.swift
+++ Project 09 - Animations/Animations/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let headerHeight = 50.0
 
+class ViewController: UIViewController {
+  // MARK: - IBOutlets
+  @IBOutlet weak var masterTableView: UITableView!
+  
+  private let sections = ["BASIC ANIMATIONS", "KEYFRAME ANIMATIONS", "MULTIPLE ANIMATIONS", "TRANSITIONS", "UIVIEW PROPERTY ANIMATIONS"]
+  private let items = [["2-Color", "2-Point Position", "Simple 2D Rotation", "3D Rotation"],
+                       ["Multicolor", "Multi Point Position", "BezierCurve Position"],
+                       ["Color and Frame Change", "Color and Border(Apple)"],
+                       ["Transitions"],
+                       ["Simple Frame Change", "Multi Frame Change"]]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
+  
+  
+  
+}
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+extension ViewController: UITableViewDelegate {
+  func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return CGFloat(headerHeight)
   }
+}
 
-
+extension ViewController: UITableViewDataSource {
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return sections.count
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return items[section].count
+  }
+  
+  func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return sections[section]
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cellIdentifier = "cell"
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+    
+    cell.textLabel?.text = items[indexPath.section][indexPath.row]
+    
+    return cell
+  }
 }
 
