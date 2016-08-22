d8f27103ffc5fffda87d798df64b863469e558bb
soapyigu <rio.deliberate@gmail.com>
2016-08-21 17:58:05 -0700

    [TwitterBird] implement twitter animation with CAKeyFrameAnimation

--- Project 13 - TwitterBird/TwitterBird/AppDelegate.swift
+++ Project 13 - TwitterBird/TwitterBird/AppDelegate.swift
@@ -12,18 +12,30 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+  var mask: CALayer?
+  var imageView: UIImageView?
   
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     window = UIWindow(frame: UIScreen.mainScreen().bounds)
     
     if let window = window {
       // add background imageView
-      let imageView = UIImageView(frame: window.frame)
-      imageView.image = UIImage(named: "startScreen")
-      window.addSubview(imageView)
+      imageView = UIImageView(frame: window.frame)
+      imageView!.image = UIImage(named: "twitterScreen")
+      window.addSubview(imageView!)
+      
+      // set up mask
+      mask = CALayer()
+      mask?.contents = UIImage(named: "twitterBird")?.CGImage
+      mask?.position = window.center
+      mask?.bounds = CGRect(x: 0, y: 0, width: 100, height: 80)
+      imageView!.layer.mask = mask
+      
+      animateMask()
       
       // make window visible
       window.rootViewController = UIViewController()
+      window.backgroundColor = UIColor(red: 70/255, green: 154/255, blue: 233/255, alpha: 1)
       window.makeKeyAndVisible()
     }
     
@@ -47,11 +59,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
   func applicationDidBecomeActive(application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the uer interface.
   }
 
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
+  
+  func animateMask() {
+    // init key frame animation
+    let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+    keyFrameAnimation.delegate = self
+    keyFrameAnimation.duration = 1
+    keyFrameAnimation.beginTime = CACurrentMediaTime() + 1
+    
+    // animate zoom in and then zoom out
+    let initalBounds = NSValue(CGRect: mask!.bounds)
+    let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 80, height: 64))
+    let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 2000, height: 2000))
+    keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
+    
+    // set up time interals
+    keyFrameAnimation.keyTimes = [0, 0.3, 1]
+    
+    // add animation to current view
+    keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+    mask!.addAnimation(keyFrameAnimation, forKey: "bounds")
+  }
+  
+  override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+    // remove mask
+    imageView?.layer.mask = nil
   }
 }
 
