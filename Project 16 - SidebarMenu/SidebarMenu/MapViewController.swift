d343903f029450002aa61bf31709b180cd93fc19
soapyigu <rio.deliberate@gmail.com>
2016-09-02 22:30:00 -0700

    [SlidebarMenu] add starter implementation for SlidebarMenu

---
+++ Project 16 - SidebarMenu/SidebarMenu/MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  SidebarMenu
+//
+//  Created by Simon Ng on 2/2/15.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
