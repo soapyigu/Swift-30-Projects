1746eedafa59e447c60b5161b94b051b22008ee8
soapyigu <rio.deliberate@gmail.com>
2016-10-26 21:52:45 -0700

    [WhatsApp] Add AllChatsViewController

---
+++ Project 27 - Whatsapp/Whatsapp/Controller/AllChatsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AllChatsViewController.swift
+//  Whatsapp
+//
+//  Copyright © 2016 Yi Gu. All rights reserved.
+//
+
+import UIKit
+
+class AllChatsViewController: UIViewController {
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
