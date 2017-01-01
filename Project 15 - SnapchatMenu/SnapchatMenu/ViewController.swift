f1cd2eb1766f72f76ceb977fc9c499a0c71bafcd
soapyigu <rio.deliberate@gmail.com>
2017-01-01 00:07:24 -0800

    [Snapchat] Optimize code style and add snap view

--- Project 15 - SnapchatMenu/SnapchatMenu/ViewController.swift
+++ Project 15 - SnapchatMenu/SnapchatMenu/ViewController.swift
@@ -20,27 +20,36 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    /// Create view controllers and add them to current view controller.
     let chatVC = UIViewController(nibName: vcName.chat.rawValue, bundle: nil)
     let storiesVC = UIViewController(nibName: vcName.stories.rawValue, bundle: nil)
     let discoverVC = UIViewController(nibName: vcName.discover.rawValue, bundle: nil)
-    
     add(childViewController: chatVC, toParentViewController: self)
     add(childViewController: storiesVC, toParentViewController: self)
     add(childViewController: discoverVC, toParentViewController: self)
     
-    var storiesFrame = storiesVC.view.frame
-    storiesFrame.origin.x = view.frame.width
-    storiesVC.view.frame = storiesFrame
+    /// Set up current view.
+    let snapView = UIImageView(image: UIImage(named: "Snap"))
+    changeX(ofView: snapView, xPosition: view.frame.width)
+    scrollView.addSubview(snapView)
     
-    var discoverFrame = discoverVC.view.frame
-    discoverFrame.origin.x = view.frame.width * 2
-    discoverVC.view.frame = discoverFrame
+    /// Move stories and discover view to the right screen.
+    changeX(ofView: storiesVC.view, xPosition: view.frame.width * 2)
+    changeX(ofView: discoverVC.view, xPosition: view.frame.width * 3)
     
-    scrollView.contentSize = CGSize(width: view.frame.width * 3, height: view.frame.height)
+    /// Set up contentSize and contentOffset.
+    scrollView.contentSize = CGSize(width: view.frame.width * 4, height: view.frame.height)
+    scrollView.contentOffset.x = view.frame.width
   }
   
   override var prefersStatusBarHidden: Bool {
     return true
+  }
+  
+  fileprivate func changeX(ofView view: UIView, xPosition: CGFloat) {
+    var frame = view.frame
+    frame.origin.x = xPosition
+    view.frame = frame
   }
   
   fileprivate func add(childViewController: UIViewController, toParentViewController parentViewController: UIViewController) {
@@ -49,4 +58,3 @@ class ViewController: UIViewController {
     childViewController.didMove(toParentViewController: parentViewController)
   }
 }
-
