20149fe7228248dae94123dc05e78927d4eaba29
soapyigu <rio.deliberate@gmail.com>
2017-01-13 11:18:06 -0800

    [UIScrollView] Handle keyboard show and hide problem

--- Project 28 - PhotoScroll/PhotoScroll/PhotoCommentViewController.swift
+++ Project 28 - PhotoScroll/PhotoScroll/PhotoCommentViewController.swift
@@ -21,22 +21,53 @@ class PhotoCommentViewController: UIViewController {
     if let photoName = photoName {
       imageView.image = UIImage(named: photoName)
     }
+    
+    let tapGesture = UITapGestureRecognizer(target: self, action: Selector.generalTap)
+    view.addGestureRecognizer(tapGesture)
+    
+    NotificationCenter.default.addObserver(
+      self,
+      selector: Selector.keyboardWillShowHandler,
+      name: NSNotification.Name.UIKeyboardWillShow,
+      object: nil
+    )
+    NotificationCenter.default.addObserver(
+      self,
+      selector: Selector.keyboardWillHideHandler,
+      name: NSNotification.Name.UIKeyboardWillHide,
+      object: nil
+    )
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  deinit {
+    NotificationCenter.default.removeObserver(self)
   }
   
+  fileprivate func adjustInsetForKeyboard(isShow: Bool, notification: Notification) {
+    guard let value = notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue else {
+      return
+    }
+    let keyboardFrame = value.cgRectValue
+    let adjustmentHeight = (keyboardFrame.height + 20) * (isShow ? 1 : -1)
+    scrollView.contentInset.bottom += adjustmentHeight
+    scrollView.scrollIndicatorInsets.bottom += adjustmentHeight
+  }
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  func dismissKeyboard() {
+    view.endEditing(true)
+  }
   
+  func keyboardWillShow(notification: Notification) {
+    adjustInsetForKeyboard(isShow: true, notification: notification)
+  }
+  
+  func keyboardWillHide(notification: Notification) {
+    adjustInsetForKeyboard(isShow: false, notification: notification)
+  }
+}
+
+fileprivate extension Selector {
+  static let keyboardWillShowHandler = #selector(PhotoCommentViewController.keyboardWillShow(notification:))
+  static let keyboardWillHideHandler = #selector(PhotoCommentViewController.keyboardWillHide(notification:))
+  static let generalTap = #selector(PhotoCommentViewController.dismissKeyboard)
 }
