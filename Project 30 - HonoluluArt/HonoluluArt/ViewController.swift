d8239d6d57478d2673bb970cc18345f7c661ab35
soapyigu <rio.deliberate@gmail.com>
2017-01-29 09:36:37 -0800

    [Mapkit] Add a new project

---
+++ Project 30 - HonoluluArt/HonoluluArt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HonoluluArt
+//
+//  Created by Yi Gu on 1/29/17.
+//  Copyright © 2017 Yi Gu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
