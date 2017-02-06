420957c8a2e564cef31b99885ede0984c1c710b2
soapyigu <rio.deliberate@gmail.com>
2017-02-05 21:34:20 -0800

    [QuoraDots] Implement animations

--- Project 14 - QuoraDots/QuoraDots/ViewController.swift
+++ Project 14 - QuoraDots/QuoraDots/ViewController.swift
@@ -2,24 +2,39 @@
 //  ViewController.swift
 //  QuoraDots
 //
-//  Created by Yi Gu on 8/23/16.
 //  Copyright © 2016 yigu. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var dotOne: UIImageView!
+  @IBOutlet weak var dotTwo: UIImageView!
+  @IBOutlet weak var dotThree: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    startAnimation()
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func startAnimation() {
+    dotOne.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    dotTwo.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    dotThree.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    
+    UIView.animate(withDuration: 0.6, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+      self.dotOne.transform = CGAffineTransform.identity
+    }, completion: nil)
+    
+    UIView.animate(withDuration: 0.6, delay: 0.2, options: [.repeat, .autoreverse], animations: {
+      self.dotTwo.transform = CGAffineTransform.identity
+    }, completion: nil)
+    
+    UIView.animate(withDuration: 0.6, delay: 0.4, options: [.repeat, .autoreverse], animations: {
+      self.dotThree.transform = CGAffineTransform.identity
+    }, completion: nil)
   }
-
-
 }
 
