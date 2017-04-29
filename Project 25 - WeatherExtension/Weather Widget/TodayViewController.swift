63d4afabecf423a0f6a2137b2978ae16f06247fd
soapyigu <rio.deliberate@gmail.com>
2017-04-29 10:52:10 -0700

    update weather widget with more concise code

--- Project 25 - WeatherExtension/Weather Widget/TodayViewController.swift
+++ Project 25 - WeatherExtension/Weather Widget/TodayViewController.swift
@@ -16,38 +16,40 @@ class TodayViewController: UIViewController, NCWidgetProviding {
   @IBOutlet weak var weatherLabel: UILabel!
   @IBOutlet weak var tempLabel: UILabel!
   
-  var location = "San Francisco, U.S."
+  fileprivate var isFetched = false
+  fileprivate var location = "San Francisco, U.S."
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view from its nib.
     self.preferredContentSize = CGSize(width: UIScreen.main.applicationFrame.size.width, height: 37.0);
+    
+    // This will be called before widgetPerformUpdate func
     displayCurrentWeather()
   }
   
-  func displayCurrentWeather() -> Bool {
+  func displayCurrentWeather() {
     // Update location
     cityLabel.text = location
-    var isFetched = false
 
     // Invoke weather service to get the weather data
     WeatherService.sharedWeatherService().getCurrentWeather(location, completion: { (data) -> () in
       OperationQueue.main.addOperation({ () -> Void in
         guard let weatherData = data else {
-          isFetched = false
+          self.isFetched = false
           return
         }
+        
+        self.isFetched = true
 
         self.weatherLabel.text = weatherData.weather.capitalized
         self.tempLabel.text = String(format: "%d", weatherData.temperature) + "\u{00B0}"
-        isFetched = true
       })
     })
-    return isFetched
   }
   
   func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-    if displayCurrentWeather() {
+    if isFetched {
       completionHandler(NCUpdateResult.newData)
     } else {
       completionHandler(NCUpdateResult.noData)
--- Project 25 - WeatherExtension/WeatherDemo/WeatherService.swift
+++ Project 25 - WeatherExtension/WeatherDemo/WeatherService.swift
@@ -9,55 +9,54 @@
 import Foundation
 
 open class WeatherService {
-    public typealias WeatherDataCompletionBlock = (_ data: WeatherData?) -> ()
-
-    let openWeatherBaseAPI = "http://api.openweathermap.org/data/2.5/weather?appid=97cce5b42320d87100a885f5dfa0dac9&units=metric&q="
-    let urlSession:URLSession = URLSession.shared
-
-    open class func sharedWeatherService() -> WeatherService {
-        return _sharedWeatherService
-    }
+  public typealias WeatherDataCompletionBlock = (_ data: WeatherData?) -> ()
+  
+  let openWeatherBaseAPI = "http://api.openweathermap.org/data/2.5/weather?appid=97cce5b42320d87100a885f5dfa0dac9&units=metric&q="
+  let urlSession:URLSession = URLSession.shared
+  
+  open class func sharedWeatherService() -> WeatherService {
+    return _sharedWeatherService
+  }
+  
+  open func getCurrentWeather(_ location:String, completion: @escaping WeatherDataCompletionBlock) {
+    let openWeatherAPI = openWeatherBaseAPI + location.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+    print(openWeatherAPI)
+    let request = URLRequest(url: URL(string: openWeatherAPI)!)
+    let weatherData = WeatherData()
     
-    open func getCurrentWeather(_ location:String, completion: @escaping WeatherDataCompletionBlock) {
-        let openWeatherAPI = openWeatherBaseAPI + location.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        print(openWeatherAPI)
-        let request = URLRequest(url: URL(string: openWeatherAPI)!)
-        let weatherData = WeatherData()
+    let task = urlSession.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
+      
+      guard let data = data else {
+        if let error = error  {
+          print(error)
+        }
         
-        let task = urlSession.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
-            
-            guard let data = data else {
-                if let error = error  {
-                    print(error)
-                }
-                
-                return
-            }
-            
-            // Retrieve JSON data
-            do {
-                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary
-                
-                // Parse JSON data
-                let jsonWeather = jsonResult?["weather"] as! [AnyObject]
-                
-                for jsonCurrentWeather in jsonWeather {
-                    weatherData.weather = jsonCurrentWeather["description"] as! String
-                }
-                
-                let jsonMain = jsonResult?["main"] as! Dictionary<String, AnyObject>
-                weatherData.temperature = jsonMain["temp"] as! Int
-                
-                completion(weatherData)
-
-            } catch {
-                print(error)
-            }
-        })
+        return
+      }
+      
+      // Retrieve JSON data
+      do {
+        let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary
         
-        task.resume()
-    }
-
+        // Parse JSON data
+        let jsonWeather = jsonResult?["weather"] as! [AnyObject]
+        
+        for jsonCurrentWeather in jsonWeather {
+          weatherData.weather = jsonCurrentWeather["description"] as! String
+        }
+        
+        let jsonMain = jsonResult?["main"] as! Dictionary<String, AnyObject>
+        weatherData.temperature = jsonMain["temp"] as! Int
+        
+        completion(weatherData)
+        
+      } catch {
+        print(error)
+      }
+    })
+    
+    task.resume()
+  }
 }
 
 let _sharedWeatherService: WeatherService = { WeatherService() }()
