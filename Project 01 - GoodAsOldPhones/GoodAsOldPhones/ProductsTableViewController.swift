6fcb579447125b46216790952426397dd120951e
Abdullah <abdullah_abd90@mail.ru>
2017-05-01 23:32:46 +0300

    Optimize code style for Project 01

--- Project 01 - GoodAsOldPhones/GoodAsOldPhones/ProductsTableViewController.swift
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/ProductsTableViewController.swift
@@ -9,32 +9,31 @@ import UIKit
 
 class ProductsTableViewController: UITableViewController {
   fileprivate var products: [Product]?
-  fileprivate let identifer = "productCell"
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    products = [Product(name: "1907 Wall Set", cellImageName: "image-cell1", fullscreenImageName: "phone-fullscreen1"),
-                Product(name: "1921 Dial Phone", cellImageName: "image-cell2", fullscreenImageName: "phone-fullscreen2"),
-                Product(name: "1937 Desk Set", cellImageName: "image-cell3", fullscreenImageName: "phone-fullscreen3"),
-                Product(name: "1984 Moto Portable", cellImageName: "image-cell4", fullscreenImageName: "phone-fullscreen4")]
+    products = [
+        Product(name: "1907 Wall Set", cellImageName: "image-cell1", fullscreenImageName: "phone-fullscreen1"),
+        Product(name: "1921 Dial Phone", cellImageName: "image-cell2", fullscreenImageName: "phone-fullscreen2"),
+        Product(name: "1937 Desk Set", cellImageName: "image-cell3", fullscreenImageName: "phone-fullscreen3"),
+        Product(name: "1984 Moto Portable", cellImageName: "image-cell4", fullscreenImageName: "phone-fullscreen4")
+    ]
   }
   
   // MARK: - UITableViewDataSource
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if let products = products {
-      return products.count
-    }
-    return 0
+    return products?.count ?? 0
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath)
     
-    let cell = tableView.dequeueReusableCell(withIdentifier: identifer, for: indexPath)
+    guard let products = products else { return cell }
     
-    cell.textLabel?.text = products?[(indexPath as NSIndexPath).row].name
+    cell.textLabel?.text = products[(indexPath as NSIndexPath).row].name
     
-    if let imageName = products?[(indexPath as NSIndexPath).row].cellImageName {
+    if let imageName = products[(indexPath as NSIndexPath).row].cellImageName {
       cell.imageView?.image = UIImage(named: imageName)
     }
     
