7ed66027132df757b98d0536ce378569d2474650
soapyigu <rio.deliberate@gmail.com>
2017-05-16 21:46:56 -0700

    Add comment and update code style

--- Project 18 - BlueLibrarySwift/BlueLibrarySwift/HTTPClient.swift
+++ Project 18 - BlueLibrarySwift/BlueLibrarySwift/HTTPClient.swift
@@ -32,11 +32,14 @@ class HTTPClient {
 		return Data() as (AnyObject)
 	}
 	
+  // This is synchronous way to download image via url, please use it in background
 	func downloadImage(_ url: String) -> (UIImage) {
 		let aUrl = URL(string: url)
-        guard let data = try? Data(contentsOf: aUrl!) else {
-            return UIImage()
-        }
+    
+    guard let data = try? Data(contentsOf: aUrl!) else {
+      return UIImage()
+    }
+    
 		let image = UIImage(data: data)
 		return image!
 	}
