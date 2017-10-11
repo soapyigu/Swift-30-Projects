cbf01083cffade0d748f465d7c9f29a4d80e0b82
soapyigu <rio.deliberate@gmail.com>
2017-10-10 23:44:07 -0700

    Update GoodAsOldPhone to support iPhone X

--- Project 01 - GoodAsOldPhones/GoodAsOldPhones/ContactViewController.swift
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/ContactViewController.swift
@@ -13,13 +13,19 @@ class ContactViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     view.addSubview(scrollView)
   }
   
-  override func viewWillLayoutSubviews() {
-    super.viewWillLayoutSubviews()
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
     
-    scrollView.contentSize = CGSize(width: 375, height: 800)
+    if #available(iOS 11.0, *) {
+      scrollView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.width, height: view.frame.height - view.safeAreaInsets.bottom)
+    } else {
+      scrollView.frame = CGRect(x: 0, y: topLayoutGuide.length, width: view.frame.width, height: view.frame.height - topLayoutGuide.length)
+    }
+    
+    scrollView.contentSize = CGSize(width: self.view.frame.width, height: 800)
   }
 }
