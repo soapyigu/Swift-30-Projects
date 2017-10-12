f2ebec2dd89598e3b6aea770764abf3d03787f7f
soapyigu <rio.deliberate@gmail.com>
2017-10-11 20:16:01 -0700

    Fix Project 1 - 3 to support iPhone X

--- Project 01 - GoodAsOldPhones/GoodAsOldPhones/ContactViewController.swift
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/ContactViewController.swift
@@ -21,9 +21,9 @@ class ContactViewController: UIViewController {
     super.viewDidLayoutSubviews()
     
     if #available(iOS 11.0, *) {
-      scrollView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.width, height: view.frame.height - view.safeAreaInsets.bottom)
+      scrollView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.width, height: view.frame.height - view.safeAreaInsets.bottom - view.safeAreaInsets.top)
     } else {
-      scrollView.frame = CGRect(x: 0, y: topLayoutGuide.length, width: view.frame.width, height: view.frame.height - topLayoutGuide.length)
+      scrollView.frame = CGRect(x: 0, y: topLayoutGuide.length, width: view.frame.width, height: view.frame.height - topLayoutGuide.length - bottomLayoutGuide.length)
     }
     
     scrollView.contentSize = CGSize(width: self.view.frame.width, height: 800)
