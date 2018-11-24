9be852a4db7a7cf35daf16b5b175902dd7e3b9bd
Rain <rain2540@foxmail.com>
2018-11-24 08:21:39 +0800

    refactor (Project 05) : update to Swift 4.2

--- Project 05 - Artistry/Artistry/ArtistDetailViewController.swift
+++ Project 05 - Artistry/Artistry/ArtistDetailViewController.swift
@@ -34,38 +34,43 @@ class ArtistDetailViewController: UIViewController {
     super.viewDidLoad()
     title = selectedArtist.name
     
-    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.rowHeight = UITableView.automaticDimension
     tableView.estimatedRowHeight = 300
     
-    NotificationCenter.default.addObserver(forName: .UIContentSizeCategoryDidChange, object: .none, queue: OperationQueue.main) { [weak self] _ in
+    NotificationCenter.default.addObserver(forName: UIContentSizeCategory.didChangeNotification, object: .none, queue: OperationQueue.main) { [weak self] _ in
       self?.tableView.reloadData()
     }
   }
 }
 
 extension ArtistDetailViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  func tableView(_ tableView: UITableView,
+                 numberOfRowsInSection section: Int) -> Int
+  {
     return selectedArtist.works.count
   }
   
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  func tableView(_ tableView: UITableView,
+                 cellForRowAt indexPath: IndexPath) -> UITableViewCell
+  {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! WorkTableViewCell
     
     let work = selectedArtist.works[indexPath.row]
     
-    cell.workTitleLabel.text = work.title
     cell.workImageView.image = work.image
     
-    cell.workTitleLabel.backgroundColor = UIColor(white: 204/255, alpha: 1)
+    cell.workTitleLabel.text = work.title
+    cell.workTitleLabel.backgroundColor = UIColor(white: 204 / 255, alpha: 1)
     cell.workTitleLabel.textAlignment = .center
-    cell.moreInfoTextView.textColor = UIColor(white: 114 / 255, alpha: 1)
+    
     cell.selectionStyle = .none
     
     cell.moreInfoTextView.text = work.isExpanded ? work.info : moreInfoText
+    cell.moreInfoTextView.textColor = UIColor(white: 114 / 255, alpha: 1)
     cell.moreInfoTextView.textAlignment = work.isExpanded ? .left : .center
     
-    cell.workTitleLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
-    cell.moreInfoTextView.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.footnote)
+    cell.workTitleLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
+    cell.moreInfoTextView.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.footnote)
     
     return cell
   }
--- Project 05 - Artistry/Artistry/ArtistListViewController.swift
+++ Project 05 - Artistry/Artistry/ArtistListViewController.swift
@@ -31,38 +31,42 @@ class ArtistListViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.rowHeight = UITableView.automaticDimension
     tableView.estimatedRowHeight = 140
     
-    NotificationCenter.default.addObserver(forName: .UIContentSizeCategoryDidChange, object: .none, queue: OperationQueue.main) { [weak self] _ in
+    NotificationCenter.default.addObserver(forName: UIContentSizeCategory.didChangeNotification, object: .none, queue: OperationQueue.main) { [weak self] _ in
       self?.tableView.reloadData()
     }
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if let destination = segue.destination as? ArtistDetailViewController,
-        let indexPath = tableView.indexPathForSelectedRow {
+      let indexPath = tableView.indexPathForSelectedRow {
       destination.selectedArtist = artists[indexPath.row]
     }
   }
 }
 
 extension ArtistListViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  func tableView(_ tableView: UITableView,
+                 numberOfRowsInSection section: Int) -> Int
+  {
     return artists.count
   }
   
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  func tableView(_ tableView: UITableView,
+                 cellForRowAt indexPath: IndexPath) -> UITableViewCell
+  {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ArtistTableViewCell
     
     let artist = artists[indexPath.row]
     
     cell.bioLabel.text = artist.bio
-    cell.bioLabel.textColor = UIColor(white: 114/255, alpha: 1)
+    cell.bioLabel.textColor = UIColor(white: 114 / 255, alpha: 1)
     
     cell.artistImageView.image = artist.image
-    cell.nameLabel.text = artist.name
     
+    cell.nameLabel.text = artist.name
     cell.nameLabel.backgroundColor = UIColor(red: 1, green: 152 / 255, blue: 0, alpha: 1)
     cell.nameLabel.textColor = UIColor.white
     cell.nameLabel.textAlignment = .center
