42b93d5ca15c2a872b7fdd882eb70e4b8f9420cc
Smartech <rain2540@foxmail.com>
2018-12-10 11:37:27 +0800

    refactor (Project 08) : update syntax

--- Project 08 - SimpleRSSReader/SimpleRSSReader/FeedParser.swift
+++ Project 08 - SimpleRSSReader/SimpleRSSReader/FeedParser.swift
@@ -7,33 +7,34 @@
 
 import Foundation
 
-class FeedParser: NSObject, XMLParserDelegate {
-  fileprivate var rssItems = [(title: String, description: String, pubDate: String)]()
+class FeedParser: NSObject {
+  private var rssItems = [(title: String, description: String, pubDate: String)]()
   
-  fileprivate var currentElement = ""
+  private var currentElement = ""
   
-  fileprivate var currentTitle = "" {
+  private var currentTitle = "" {
     didSet {
       currentTitle = currentTitle.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
   }
   
-  fileprivate var currentDescription = "" {
+  private var currentDescription = "" {
     didSet {
       currentDescription = currentDescription.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
   }
   
-  fileprivate var currentPubDate = "" {
+  private var currentPubDate = "" {
     didSet {
       currentPubDate = currentPubDate.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
   }
   
-  fileprivate var parserCompletionHandler: (([(title: String, description: String, pubDate: String)]) -> Void)?
+  private var parserCompletionHandler: (([(title: String, description: String, pubDate: String)]) -> Void)?
   
-  func parseFeed(feedURL: String, completionHandler: (([(title: String, description: String, pubDate: String)]) -> Void)?) -> Void {
-    
+  func parseFeed(feedURL: String,
+                 completionHandler: (([(title: String, description: String, pubDate: String)]) -> Void)?) -> Void
+  {
     parserCompletionHandler = completionHandler
     
     guard let feedURL = URL(string:feedURL) else {
@@ -58,47 +59,64 @@ class FeedParser: NSObject, XMLParserDelegate {
     }).resume()
   }
   
-  // MARK: - XMLParser Delegate
-  func parserDidStartDocument(_ parser: XMLParser) {
-    rssItems.removeAll()
-  }
-  
-  func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-    currentElement = elementName
-    
-    if currentElement == "item" {
-      currentTitle = ""
-      currentDescription = ""
-      currentPubDate = ""
+
+}
+
+// MARK: - XMLParser Delegate
+extension FeedParser: XMLParserDelegate {
+    func parserDidStartDocument(_ parser: XMLParser) {
+        rssItems.removeAll()
     }
-  }
-  
-  func parser(_ parser: XMLParser, foundCharacters string: String) {
-    /// Note: current string may only contain part of info.
-    switch currentElement {
-    case "title":
-      currentTitle += string
-    case "description":
-      currentDescription += string
-    case "pubDate":
-      currentPubDate += string
-    default:
-      break
+
+    func parser(_ parser: XMLParser,
+                didStartElement elementName: String,
+                namespaceURI: String?,
+                qualifiedName qName: String?,
+                attributes attributeDict: [String : String] = [:])
+    {
+        currentElement = elementName
+
+        if currentElement == "item" {
+            currentTitle = ""
+            currentDescription = ""
+            currentPubDate = ""
+        }
     }
-  }
-  
-  func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-    if elementName == "item" {
-      let rssItem = (title: currentTitle, description: currentDescription, pubDate: currentPubDate)
-      rssItems.append(rssItem)
+
+    func parser(_ parser: XMLParser,
+                foundCharacters string: String)
+    {
+        /// Note: current string may only contain part of info.
+        switch currentElement {
+        case "title":
+            currentTitle += string
+        case "description":
+            currentDescription += string
+        case "pubDate":
+            currentPubDate += string
+        default:
+            break
+        }
     }
-  }
-  
-  func parserDidEndDocument(_ parser: XMLParser) {
-    parserCompletionHandler?(rssItems)
-  }
-  
-  func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
-    print(parseError.localizedDescription)
-  }
+
+    func parser(_ parser: XMLParser,
+                didEndElement elementName: String,
+                namespaceURI: String?,
+                qualifiedName qName: String?)
+    {
+        if elementName == "item" {
+            let rssItem = (title: currentTitle, description: currentDescription, pubDate: currentPubDate)
+            rssItems.append(rssItem)
+        }
+    }
+
+    func parserDidEndDocument(_ parser: XMLParser) {
+        parserCompletionHandler?(rssItems)
+    }
+
+    func parser(_ parser: XMLParser,
+                parseErrorOccurred parseError: Error)
+    {
+        print(parseError.localizedDescription)
+    }
 }
--- Project 08 - SimpleRSSReader/SimpleRSSReader/NewsTableViewController.swift
+++ Project 08 - SimpleRSSReader/SimpleRSSReader/NewsTableViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class NewsTableViewController: UITableViewController {
   
-  fileprivate let feedParser = FeedParser()
-  fileprivate let feedURL = "http://www.apple.com/main/rss/hotnews/hotnews.rss"
+  private let feedParser = FeedParser()
+  private let feedURL = "http://www.apple.com/main/rss/hotnews/hotnews.rss"
   
-  fileprivate var rssItems: [(title: String, description: String, pubDate: String)]?
-  fileprivate var cellStates: [CellState]?
+  private var rssItems: [(title: String, description: String, pubDate: String)]?
+  private var cellStates: [CellState]?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -32,43 +32,52 @@ class NewsTableViewController: UITableViewController {
       }
     }
   }
-  
-  // MARK: - Table view data source
-  
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
-  
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    guard let rssItems = rssItems else {
-      return 0
+}
+
+// MARK: - Table view data source
+extension NewsTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    return rssItems.count
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsTableViewCell
-    
-    if let item = rssItems?[indexPath.row] {
-      (cell.titleLabel.text, cell.descriptionLabel.text, cell.dateLabel.text) = (item.title, item.description, item.pubDate)
-      
-      if let cellState = cellStates?[indexPath.row] {
-        cell.descriptionLabel.numberOfLines = cellState == .expanded ? 0: 4
-      }
+
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int
+    {
+        guard let rssItems = rssItems else {
+            return 0
+        }
+        return rssItems.count
     }
-    
-    return cell
-  }
-  
-  // MARK: - Table view delegate
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    tableView.deselectRow(at: indexPath, animated: true)
-    
-    let cell = tableView.cellForRow(at: indexPath) as! NewsTableViewCell
-    
-    tableView.beginUpdates()
-    cell.descriptionLabel.numberOfLines = cell.descriptionLabel.numberOfLines == 4 ? 0 : 4
-    cellStates?[indexPath.row] = cell.descriptionLabel.numberOfLines == 4 ? .collapsed : .expanded
-    tableView.endUpdates()
-  }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsTableViewCell
+
+        if let item = rssItems?[indexPath.row] {
+            (cell.titleLabel.text, cell.descriptionLabel.text, cell.dateLabel.text) = (item.title, item.description, item.pubDate)
+
+            if let cellState = cellStates?[indexPath.row] {
+                cell.descriptionLabel.numberOfLines = cellState == .expanded ? 0: 4
+            }
+        }
+
+        return cell
+    }
+}
+
+// MARK: - Table view delegate
+extension NewsTableViewController {
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let cell = tableView.cellForRow(at: indexPath) as! NewsTableViewCell
+
+        tableView.beginUpdates()
+        cell.descriptionLabel.numberOfLines = cell.descriptionLabel.numberOfLines == 4 ? 0 : 4
+        cellStates?[indexPath.row] = cell.descriptionLabel.numberOfLines == 4 ? .collapsed : .expanded
+        tableView.endUpdates()
+    }
 }
