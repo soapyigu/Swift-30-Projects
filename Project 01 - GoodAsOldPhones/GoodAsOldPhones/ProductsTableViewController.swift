e7a9ac0767af28e2682210d05bfc2ff3d025af31
jamelwrottenaoiths <jamelwrotten@gmail.com>
2019-11-14 16:57:26 -0500

    One change

--- Project 01 - GoodAsOldPhones/GoodAsOldPhones/ProductsTableViewController.swift
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/ProductsTableViewController.swift
@@ -15,7 +15,7 @@ class ProductsTableViewController: UITableViewController {
     super.viewDidLoad()
     
     products = [
-      Product(name: "1907 Wall Set", cellImageName: "image-cell1", fullscreenImageName: "phone-fullscreen1"),
+      Product(name: "jamal Wall Set", cellImageName: "image-cell1", fullscreenImageName: "phone-fullscreen1"),
       Product(name: "1921 Dial Phone", cellImageName: "image-cell2", fullscreenImageName: "phone-fullscreen2"),
       Product(name: "1937 Desk Set", cellImageName: "image-cell3", fullscreenImageName: "phone-fullscreen3"),
       Product(name: "1984 Moto Portable", cellImageName: "image-cell4", fullscreenImageName: "phone-fullscreen4")
